ac738628ba4435fe77678bd03b12f6e8ce14bc9d
jackob147 <36490578+jackob147@users.noreply.github.com>
2018-02-14 23:16:14 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
+import os
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
-import os
+
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = "!")
