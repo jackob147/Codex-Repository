20dd69fbe2ee88c2e1069a94905dd1cc89bd7fb2
jackob147 <36490578+jackob147@users.noreply.github.com>
2019-01-29 13:26:13 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -261,7 +261,7 @@ async def on_message(message):
         await client.send_message(message.channel, "As partes do Warframe Nyx dropam do inimigo Phorid, durante uma invasão infestada. \n https://www.youtube.com/watch?v=49ONrX1Xzl0&index=20&list=PLcHK4Prg02jdfwmrYZPG_RVGTyT2RYVPF")
 
     if "!BARUUK" in message.content.upper():
-        await client.send_message(message.channel, "As partes e o diagrama principal do Warframe Baruuk podem ser compradas com a Little Duck, usando pontos Vox Soalaris, em Fortuna. \n https://www.youtube.com/watch?v=If4hJNAf_AE&feature=youtu.be&list=PLDmrv5FhAWj_VzURMqylYnfxlRWvtLspv")
+        await client.send_message(message.channel, "As partes e o diagrama principal do Warframe Baruuk podem ser compradas com a Little Duck, usando pontos Vox Solaris, em Fortuna. \n https://www.youtube.com/watch?v=If4hJNAf_AE&feature=youtu.be&list=PLDmrv5FhAWj_VzURMqylYnfxlRWvtLspv")
 
     if "!GARUDA" in message.content.upper():
         await client.send_message(message.channel, "O diagrama principal da Garuda é recompensa da jornada Vox Solaris, e seus diagramas podem ser conseguidos nas Caças de Vallis. \n https://www.youtube.com/watch?v=DH2bSFOBsdM&feature=youtu.be&list=PLDmrv5FhAWj_VzURMqylYnfxlRWvtLspv")
